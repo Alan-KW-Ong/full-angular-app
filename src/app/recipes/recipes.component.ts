96479b9eb19cb1b43e1df052bdbfa675364e9478
Alan-KW-Ong <alan37okw@gmail.com>
2022-04-30 14:37:11 +0800

    adding content to recipes components

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from './recipe.model';
+ 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg')
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
