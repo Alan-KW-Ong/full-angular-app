7d8ecd16f25d28dbf588974b75cad50be80cb415
Alan-KW-Ong <alan37okw@gmail.com>
2022-05-07 10:50:44 +0800

    adding logging services and put them into component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoggingService } from './shared/logging.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [LoggingService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'full-angular-app';
   oddNumbers: number[] = [];
   evenNumbers: number[] = [];
@@ -14,9 +16,17 @@ export class AppComponent {
   onIntervalFired(firedNumber: number) {
     if (firedNumber % 2 === 0) {
       this.evenNumbers.push(firedNumber);
+      this.loggingService.logStatus(String(firedNumber));
     } else {
       this.oddNumbers.push(firedNumber);
+      this.loggingService.logStatus(String(firedNumber));
     }
+  }
+
+  constructor(private loggingService: LoggingService) { }
+
+  ngOnInit(): void {
+    
   }
   
 }
---
+++ src/app/shared/logging.service.ts
@@ -0,0 +1,8 @@
+export class LoggingService {
+    
+    logStatus(message: string)
+    {
+        console.log(message);  
+    }
+    
+}
